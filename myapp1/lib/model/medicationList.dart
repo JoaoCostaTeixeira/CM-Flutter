6e9f71b05c7719481c026629e6dd99434bcdfdf9
JoaoTeixeira <joaocostateixeira@ua.pt>
2020-04-06 18:41:14 +0100

    lista de medicamentos e funçao que retorna apenas 1 para ligar QR code

---
+++ myapp1/lib/model/medicationList.dart
@@ -0,0 +1,15 @@
+class Medication {
+  final int id;
+  final String name;
+  final int size;
+  final String image;
+
+  Medication({this.id, this.name, this.size, this.image});
+}
+
+class ListMed{
+    final List<Medication> listMed = {
+      Medication(id:1 , name: "Ben-u-ron 500g", size: 20,  image:"https://www.farmaciasportuguesas.pt/catalogo/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/8/1/816861720161010125245_1.jpg" ),
+    };
+
+}
